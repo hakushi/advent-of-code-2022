7f5494d8e5154c5af1cbc7269e5bbaa990f8ad0a
David Edström <hakushi242@gmail.com>
2022-12-01 19:49:50 +0100

    day 1 part 2

---
+++ 01-2.js
@@ -0,0 +1,23 @@
+const data = require('./01-data.js')
+
+const testData = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+const sorted = data
+  .split('\n\n')
+  .map(item => item.split('\n')
+  .map(item => parseInt(item))
+  .reduce((a, b) => a+b)).sort((a, b) => a-b)
+console.log(sorted.slice(sorted.length-3).reduce((a, b) => a+))
